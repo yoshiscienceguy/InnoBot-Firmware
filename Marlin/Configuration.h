cbcce776a7e9ee9eca44de742f93482fb96c54d2
Shoeb Memon <mshoeb13@gmail.com>
2018-12-26 19:46:28 -0800

    Test
    added a comment

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -131,7 +131,7 @@
 // The following define selects which electronics board you have.
 // Please choose the name from boards.h that matches your setup
 #ifndef MOTHERBOARD
-  #define MOTHERBOARD BOARD_MKS_GEN_13 // fdp
+  #define MOTHERBOARD BOARD_MKS_GEN_13 // fdp //shoeb
 #endif
 
 // Optional custom name for your RepStrap or other custom machine
