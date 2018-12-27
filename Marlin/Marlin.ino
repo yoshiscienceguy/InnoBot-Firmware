33b4a31d69c10cf240fdb052455bac79a2bced1d
Fernando de Paz <f.depaz@hotmail.com>
2018-12-26 19:48:20 -0800

    test2
    yay

--- Marlin/Marlin.ino
+++ Marlin/Marlin.ino
@@ -17,7 +17,7 @@ see if there's a more suitable starting-point for your specific hardware.
 
 Before diving in, we recommend the following essential links:
 
-Marlin Firmware Official Website
+Marlin Firmware Official Website //important
 
   - http://marlinfw.org/
     The official Marlin Firmware website contains the most up-to-date
