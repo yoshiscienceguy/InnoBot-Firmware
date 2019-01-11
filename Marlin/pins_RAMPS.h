cd9efd366979347a71ed434a62a7a8464cde848e
Fernando de Paz <f.depaz@hotmail.com>
2019-01-10 17:15:16 -0800

    Changes for TMC2130

--- Marlin/pins_RAMPS.h
+++ Marlin/pins_RAMPS.h
@@ -128,13 +128,13 @@
  */
 #if ENABLED(TMC_USE_SW_SPI)
   #ifndef TMC_SW_MOSI
-    #define TMC_SW_MOSI    66
+    #define TMC_SW_MOSI    51
   #endif
   #ifndef TMC_SW_MISO
-    #define TMC_SW_MISO    44
+    #define TMC_SW_MISO    50
   #endif
   #ifndef TMC_SW_SCK
-    #define TMC_SW_SCK     64
+    #define TMC_SW_SCK     52
   #endif
 #endif
 
