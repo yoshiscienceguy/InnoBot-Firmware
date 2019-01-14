036efea67abd32e00d9a18bf0515d16be87365c8
Fernando de Paz <f.depaz@hotmail.com>
2019-01-13 17:06:28 -0800

    Fixed sensitivity
    Sensitivity too low.

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -382,8 +382,8 @@
 // @section homing
 
 // Homing hits each endstop, retracts by these distances, then does a slower bump.
-#define X_HOME_BUMP_MM 0  //5
-#define Y_HOME_BUMP_MM 0  //shoeb 5
+#define X_HOME_BUMP_MM 5  //5
+#define Y_HOME_BUMP_MM 5  //shoeb 5
 #define Z_HOME_BUMP_MM 2  
 #define HOMING_BUMP_DIVISOR { 2, 2, 4 }  // Re-Bump Speed Divisor (Divides the Homing Feedrate)
 //#define QUICK_HOME                     // If homing includes X and Y, do a diagonal move initially
@@ -1205,9 +1205,9 @@
   #define SENSORLESS_HOMING // TMC2130 only //shoeb
 
   #if ENABLED(SENSORLESS_HOMING)
-    #define X_HOMING_SENSITIVITY  8
-    #define Y_HOMING_SENSITIVITY  8 
-    #define Z_HOMING_SENSITIVITY  2
+    #define X_HOMING_SENSITIVITY  13
+    #define Y_HOMING_SENSITIVITY  10
+    //#define Z_HOMING_SENSITIVITY  8
   #endif
 
   /**
